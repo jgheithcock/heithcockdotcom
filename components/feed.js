7b6844c1cefa58143b29367e86239b3bf4ff53c5
JG Heithcock <jgheithcock@gmail.com>
2022-10-06 23:19:28 -0700

    Better weather

--- components/feed.js
+++ components/feed.js
@@ -2,9 +2,9 @@ import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import Content from "./content";
 
-export default function Feed({ title, url, ...props }) {
+export default function Feed({ feed_id, title, url, ...props }) {
   return (
-    <div className={styles.card}>
+    <div className={`${styles.card} ${feed_id}`}>
       <h2>
         <Link key={"url"} href={url}>
           {title}
