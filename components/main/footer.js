b511db93c7c1fa34439e8d7dd74f18ccff39a987
JG Heithcock <jgheithcock@gmail.com>
2026-09-13 20:49:46 -0700

    Fix up print styling
    Adds path to bottom of footer in front of copyright notice

--- components/main/footer.js
+++ components/main/footer.js
@@ -1,12 +1,16 @@
 import Link from "next/link";
-// import Image from "next/image";
+import { useRouter } from "next/router";
 import styles from "../../styles/Main.module.css";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
+  const { asPath } = useRouter();
+  const path = asPath.split("?")[0].split("#")[0];
+
   return (
     <footer className={`${styles.footer} chrome`}>
       <p>
+        <span className={styles.printUrl}>heithcock.com{path} | </span>
         Copyright © <Link href="/about">JG Heithcock</Link> {currentYear}
       </p>
     </footer>
